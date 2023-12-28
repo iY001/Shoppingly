0085fc3695839827a4a706ded57800f6780d7732
iY001 <youssefabdo4005@gmail.com>
2023-12-28 12:41:30 +0200

    registration --responsive

--- src/Pages/registration/SignIn.jsx
+++ src/Pages/registration/SignIn.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import './css/Sign in - CSS.css'
+import { Link } from 'react-router-dom'
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 function SignIn() {
     return (
         <>
-        <div className='layout-signin'>
+            <div className='layout-signin md:w-full md:px-0 px-6'>
 
-            <div className="box">
-                <div className="container-s">
-                    <h2 style={{ marginTop: 10 }}>Sign in</h2>
-                    <div className="inputbox">
-                        <input type="email" required="required" />
-                        <span>Email</span>
-                        <line />
+                <div className="box">
+                    <div className="container-s">
+                        <div className=''>
+                            <Link to="/"  className={`z-30 absolute md:left-10 mt-3 text-xl text-[#817D81] md:hover:text-[#f43c09a7] duration-300 w-8 cursor-pointer `}>
+                                <IoMdArrowRoundBack />
+                            </Link>
+                            <h2 style={{ marginTop: 10 }}>Sign in</h2>
+                        </div>
+                        <div className="inputbox">
+                            <input type="email" required="required" />
+                            <span>Email</span>
+                            <line />
+                        </div>
+                        <div className="inputbox">
+                            <input type="password" required="required" />
+                            <span>Password</span>
+                            <line />
+                        </div>
+                        <div className="links flex flex-col">
+                            <a href="#">Forget Password</a>
+                            <a href="/signup" className='text-[#817D81]'>Sign Up</a> {/* TO REGISTER PAGE */}
+                        </div>
+                        <input onclick="window.location.href= '/sign' " type="submit" defaultValue="Login" style={{ marginTop: 5 }} />
                     </div>
-                    <div className="inputbox">
-                        <input type="password" required="required" />
-                        <span>Password</span>
-                        <line />
-                    </div>
-                    <div className="links">
-                        <a href="#">Forget Password</a>
-                        <a href="/signup">Sign Up</a>
-                        {/* TO REGISTER PAGE */}
-                    </div>
-                    <input onclick="window.location.href= '/sign' " type="submit" defaultValue="Login" style={{ marginTop: 30 }} />
                 </div>
             </div>
-        </div>
 
         </>
     )
--- src/Pages/registration/SignUp.jsx
+++ src/Pages/registration/SignUp.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './css/Sign up - CSS.css'
+import { Link } from 'react-router-dom'
+import { IoMdArrowRoundBack } from "react-icons/io";
+
 function SignUp() {
   return (
     <>
-      <div className='layout-signup'>
+      <div className='layout-signup md:w-full md:px-0 px-6'>
         <div className="box-signup">
           <div className="container-s">
-            <h2 style={{ marginTop: 10 }}>Sign Up</h2>
+            <div className=''>
+              <Link to="/" className={`z-30 absolute md:left-10 mt-3 text-xl text-[#817D81] md:hover:text-[#f43c09a7] duration-300 w-8 cursor-pointer `}>
+                <IoMdArrowRoundBack />
+              </Link>
+              <h2 style={{ marginTop: 10 }}>Sign Up</h2>
+            </div>
             <div className="inputbox">
               <input type="email" required="required" />
               <span>Username</span>
@@ -23,8 +31,7 @@ function SignUp() {
               <line />
             </div>
             <div className="links">
-              <a href="/signin">Sign in</a>
-              {/* TO REGISTER PAGE */}
+              <a href="/signin">Sign in</a> {/* TO LOGIN PAGE */}
             </div>
             <input onclick="window.location.href = 'Sign in - Page.html' " type="submit" defaultValue="Login" style={{ marginTop: 30 }} />
           </div>
